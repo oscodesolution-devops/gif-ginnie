85abbcd86deaa8d65258e37b2adf57abffbde4cd
Shiv Pratap <sp09singhwaghel@gmail.com>
2025-02-13 18:20:06 +0530

    change : loading skeleton of banner

--- src/components/SlidingImages/SkeletonLoading.tsx
+++ src/components/SlidingImages/SkeletonLoading.tsx
@@ -1,23 +1,25 @@
-
 const CarouselImagesLoading = () => {
-  const placeholderCount = 8; // Number of placeholders to match the scrolling layout
-
   return (
-    <div className="w-full relative overflow-hidden">
-      <div
-        className="flex gap-4 px-4 md:px-8 animate-pulse"
-        style={{
-          width: `calc(${placeholderCount} * 200px)`,
-        }}
-      >
-        {/* Placeholder skeletons */}
-        {Array.from({ length: placeholderCount }).map((_, index) => (
-          <div
-            key={index}
-            className="relative flex-shrink-0 w-[200px] h-[300px] rounded-lg bg-gray-300"
-          ></div>
+    <div className="relative w-full px-4 sm:px-16 md:px-24 mb-28 animate-pulse">
+      {/* Skeleton Wrapper */}
+      <div className="relative h-56 overflow-hidden rounded-lg md:h-96 bg-gray-300 dark:bg-gray-700"></div>
+
+      {/* Skeleton Indicators */}
+      <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3">
+        {[...Array(5)].map((_, index) => (
+          <div key={index} className="w-3 h-3 rounded-full bg-gray-400"></div>
         ))}
       </div>
+
+      {/* Skeleton Controls */}
+      <button
+        type="button"
+        className="absolute top-1/2 left-0 z-30 flex items-center justify-center h-10 w-10 transform -translate-y-1/2 cursor-pointer bg-gray-400 rounded-full"
+      ></button>
+      <button
+        type="button"
+        className="absolute top-1/2 right-0 z-30 flex items-center justify-center h-10 w-10 transform -translate-y-1/2 cursor-pointer bg-gray-400 rounded-full"
+      ></button>
     </div>
   );
 };
