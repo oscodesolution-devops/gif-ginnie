d1246c4024f9371dfef59f1ecdb0e5db071e781d
Shiv Pratap <sp09singhwaghel@gmail.com>
2025-02-13 18:08:56 +0530

    remove: the welcome part on main page

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,13 +3,13 @@ import ImageGallery from "../SlidingImages/SlidingImages";
 
 export default function Hero() {
   return (
-    <div className="w-full pt-12 pb-20 dark:text-white">
-      <div className="w-full text-center uppercase text-sm mb-4">
+    <div className="w-full pt-12 dark:text-white mt-6">
+      {/* <div className="w-full text-center uppercase text-sm mb-4">
         Welcome to
       </div>
       <div className="w-full text-center uppercase font-bold text-3xl px-4 sm:text-4xl mb-14">
         {name}
-      </div>
+      </div> */}
       <ImageGallery />
     </div>
   );
