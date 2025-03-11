9564e820330f93014dffa644c8a934227020cd19
Anant Kumar <anant1042005@gmail.com>
2025-03-11 12:37:36 +0530

    rect::welcome and redirection to whatsapp added

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -2,10 +2,17 @@ import BannerImg from '../../assets/banner-gift-gnnie-website.jpg'
 
 
 export default function Banner() {
+    const phoneNumber = "8000932933";
+    const message = "Hello! I'm interested in your services.";
+    const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+        message
+    )}`;
     return (
-        <div className="w-full h-[50vh] relative p-4">
+        <div className="w-full lg:h-[55vh] relative p-4">
         <div className='w-full h-full relative rounded-xl overflow-hidden'>
-            <img src={BannerImg} alt="GNNie's Gift Banner" className="w-full h-full lg:object-fit object-contain" />
+            <a href={whatsappLink} className='lg:w-full lg:h-full'>
+                <img src={BannerImg} alt="GNNie's Gift Banner" className="w-full h-full lg:object-cover object-contain" />
+            </a>
         </div>
         </div>
     );
